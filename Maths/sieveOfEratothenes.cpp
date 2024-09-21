1285ffba2ee217ba9e0d1bbf8cf0ee128f3ae912
Zeeshan <zishuz44@gmail.com>
2024-09-21 23:20:56 +0530

    add new file

--- Maths/sieveOfEratothenes.cpp
+++ Maths/sieveOfEratothenes.cpp
@@ -23,3 +23,29 @@ vector<int> findprime(int n)
 
     return primes;
 }
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> findprime(int n)
+{
+    vector<int> prime(n, 1);
+
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (prime[i] == 1)
+        {
+            for (int j = i * i; j <= n; j += i)
+            {
+                prime[j] = 0;
+            }
+        }
+    }
+    vector<int> primes;
+    for (int i = 2; i <= n; i++)
+        if (prime[i] == 1)
+            primes.push_back(i); // all prime numbers
+
+    return primes;
+}
