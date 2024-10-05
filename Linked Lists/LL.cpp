0e53d85e964975d533c2eac5eced7b288410ce90
Zeeshan <zishuz44@gmail.com>
2024-10-05 21:34:05 +0530

    add LL DS

---
+++ Linked Lists/LL.cpp
@@ -0,0 +1,18 @@
+class Node
+{
+private:
+    int data;
+    Node *next;
+
+public:
+    Node(int data)
+    {
+        this->data = data;
+        this->next = nullptr;
+    }
+    Node(int data, Node *next)
+    {
+        this->data = data;
+        this->next = next;
+    }
+};
