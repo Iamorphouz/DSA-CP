ada75f52eb102c794d41dc78517c06b99b63056f
Zeeshan <zishuz44@gmail.com>
2024-12-18 11:52:10 +0530

    add more patterns till 15

--- Print Patterns/printPattern.cpp
+++ Print Patterns/printPattern.cpp
@@ -198,14 +198,129 @@ void print9(int n)
         cout << endl;
     }
 };
-void print(int n) {
-
+void print10(int n)
+{
+    // *
+    // **
+    // ***
+    // ****
+    // *****
+    // ****
+    // ***
+    // **
+    // *
+    for (int i = 1; i <= 2 * n - 1; i++)
+    {
+        int stars = 0;
+        if (i <= n)
+            stars = i;
+        else
+            stars = 2 * n - i;
+        for (int j = 1; j <= stars; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+};
+void print11(int n)
+{
+    // 1
+    // 01
+    // 101
+    // 0101
+    // 10101
+    for (int i = 1; i <= n; i++)
+    {
+        int one = 0;
+        if (i & 1)
+            one = 1;
+        for (int j = 1; j <= i; j++)
+        {
+            cout << one;
+            one = 1 - one;
+        }
+        cout << endl;
+    }
+};
+void print12(int n)
+{
+    // 1        1
+    // 12      21
+    // 123    321
+    // 1234  4321
+    // 1234554321
+    int space = 2 * n - 2;
+    for (int i = 1; i <= n; i++)
+    {
+        // num
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j;
+        }
+        // space
+        for (int j = 1; j <= space; j++)
+        {
+            cout << " ";
+        }
+        // num
+        for (int j = i; j >= 1; j--)
+        {
+            cout << j;
+        }
+        space -= 2;
+        cout << endl;
+    }
+};
+void print13(int n)
+{
+    // 1
+    // 2 3
+    // 4 5 6
+    // 7 8 9 10
+    // 11 12 13 14 15
+    int num = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << num++ << " ";
+        }
+        cout << endl;
+    }
+};
+void print14(int n)
+{
+    // A
+    // AB
+    // ABC
+    // ABCD
+    // ABCDE
+    for (int i = 1; i <= n; i++)
+    {
+        for (char j = 'A'; j <= 'A' + i - 1; j++)
+        {
+            cout << j;
+        }
+        cout << endl;
+    }
+};
+void print15(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (char j = 'A'; j <= 'A' + (n - i); j++)
+        {
+            cout << j;
+        }
+        cout << endl;
+    }
 };
 
 int main()
 {
-    int n = 9;
+    int n = 5;
     // cin >> n;
-    print9(n);
+    print15(n);
     return 0;
 }
