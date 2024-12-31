a4d9cd5e7e36d0b6c1b51665dee5ce518d93c716
Zeeshan <zishuz44@gmail.com>
2024-12-31 22:30:10 +0530

    add leetcode 983

---
+++ DP/983_Minimum_Cost_For_Tickets.cpp
@@ -0,0 +1,17 @@
+def solve(i : int, days : List[int], costs : List[int], dp : List[int]) -> int : n = len(days) if i >= n : return 0
+
+                                                                                                           if dp[i] !=
+                                                                                     -1 : return dp[i]
+
+                                                                                          cost_1day = costs[0] + solve(i + 1, days, costs, dp)
+
+                                                                                                                     j = i while j < n and days[j] < days[i] + 7 : j
+    += 1 cost_7day = costs[1] + solve(j, days, costs, dp)
+
+                                    j = i while j < n and days[j] < days[i] + 30 : j
+    += 1 cost_30day = costs[2] + solve(j, days, costs, dp)
+
+                                     dp[i] = min(cost_1day, cost_7day, cost_30day) return dp[i]
+
+                                             class Solution : def mincostTickets(self, days : List[int], costs : List[int])
+                                                                  ->int : dp = [-1] * 366 return solve(0, days, costs, dp)
